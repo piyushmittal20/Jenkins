1a8978b13bd747e785778bf73fb3abaf380e8f22
piyushmittal20 <20piyushmittal@gmail.com>
2021-08-21 15:43:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello from Piyush and now you are in with the help of Jenkins.");
+});
+
+app.listen(5000, (req, res) => {
+  console.log(`Server is running on PORT 5000`);
+});
